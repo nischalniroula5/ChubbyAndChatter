aa21e3f2324ea1e43b30eba7c529ed1c2f0d9b9f
Nischal Niroula <officialnischalniroula@gmail.com>
2023-08-23 02:28:42 +1000

    Initial Commit

---
+++ TikTacToe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TikTacToe
+//
+//  Created by Nischal Niroula on 23/8/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TikTacToe/TikTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTacToeApp.swift
+//  TikTacToe
+//
+//  Created by Nischal Niroula on 23/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct TikTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
